2e381d3635196069e337e054888e91b3e3944260
CodeWithZareef <mzareefbhutto@gmail.com>
2025-05-12 17:39:27 +0500

    Initialize

---
+++ app.js
@@ -0,0 +1,29 @@
+
+console.log("Hello World!");
+
+// Event Loop
+
+// 1. Sync code
+
+// 2. Async code 
+
+
+// setTimeout(function, duration);
+
+// eventlistener
+
+
+// Blocking
+
+
+// Event Loop
+
+// 1. Call Stack
+// 2. Browser
+// 3. Callback/Task Queue
+
+
+// loupe tool
+
+// Single Threded Language
+
